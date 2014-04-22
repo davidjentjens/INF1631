6593906853b38a31b4236ad45f3e811b7f63dc27
Guilherme Berger <guilherme.berger@gmail.com>
2014-04-22 14:16:39 -0300

    modify benchmark to run for 5 seconds

--- T1/1.py
+++ T1/1.py
@@ -1,7 +1,10 @@
+# Implementacao do algoritmo
 def quo(x, y, k):
     if k == 1:
         return 1
     return x**(k-1) + y*quo(x, y, k-1)
+
+
 
 if __name__ == '__main__':
     # Codigo usado para testar com diferentes valores de x, y, e k
@@ -10,21 +13,36 @@ if __name__ == '__main__':
     #     x - valor de x
     #     y - valor de y
     #     k - valor de k
-    #     n - numero de execucoes
     
-    import timeit
+    from time import time
     import sys
 
-    x = sys.argv[1] 
-    y = sys.argv[2] 
-    k = sys.argv[3] 
-    number = int(sys.argv[4]) 
+    EXECS_PER_LOOP = 100000
+    TIME_TRESHOLD = 5
 
-    t = timeit.timeit("quo(" + ','.join([x, y, k]) + ")", setup = "from __main__ import quo", number = number)
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    k = int(sys.argv[3])
 
-    print("x = " + x)    
-    print("y = " + y)    
-    print("k = " + k)    
-    print("execs = " + str(number))    
-    print("time = " + ("%.3f" % t) + ' s')
-    print("time/exec = " + ("%.6f" % (1000*t/number)) + ' ms')
+    # Executa 100000 vezes a cada vez, ate passar de 5 segundos
+    start = time()
+    execs = 0
+    while time() - start < TIME_TRESHOLD:
+        execs += EXECS_PER_LOOP
+        for i in range(EXECS_PER_LOOP):
+            quo(x, y, k)
+    end = time()
+    elapsed = end - start
+
+    # Print style 1: human-readable
+    print("x = %d" % x)    
+    print("y = %d" % y)    
+    print("k = %d" % k)    
+    print("execs = %d" % execs)    
+    print("time = %.3f s" % elapsed)
+    print("time/exec = %.6f ms" % (1000*elapsed/execs))
+    print()
+
+    # Print style 2: LaTeX table
+    # print("%d & %d & %d & %d & %.3f s & %.6f ms \\\\" % 
+    #     (x, y, k, execs, elapsed, 1000*elapsed/execs))
